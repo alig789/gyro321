103a8fa7844d77fd86411927d6d4799a8bc8fa50
ali_vs <alig789@gmail.com>
2022-03-02 17:12:04 -0500

    changed directories

--- app.js
+++ app.js
@@ -1,41 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+   
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(express.static('public'));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
 });
 
-module.exports = app;
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
---
+++ public/script.js
@@ -0,0 +1,28 @@
+//Javascript file script.js 
+//CS321 Gyroscope project
+//
+//This file will be the script used to direct mobile users to the gyroscope file.
+//Any non-mobile users are not supposed to be visiting this file.
+//
+//
+
+
+//We do not necessarily need to validate for mobile/desktop clients, the scripts will just record null values for non-mobile users.
+
+//Initialize the Gyroscope object for the mobile user
+let gyroObject = new Gyroscope(0);
+let display = new displayCanvas();
+let frameCounter = 0;
+
+//This is the recursive function that will keep updating the gyroscope and display canvas
+function runGyroscope() {
+    gyroObject.update();
+    display.update();
+    frameCounter++;
+    requestAnimationFrame(runGyroscope);
+}
+
+runGyroscope();
+
+
+
