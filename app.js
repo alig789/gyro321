74bb9990071a5972ed24734ddb336d562d594c25
ali_vs <alig789@gmail.com>
2022-03-02 17:48:30 -0500

    fell back to old app.js

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-
 const express = require('express');
-
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-   
-
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
-
-
 
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
 });
 
-http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+
+
+server.listen(port, () => {
+    console.log('listening on *:3000');
 });
